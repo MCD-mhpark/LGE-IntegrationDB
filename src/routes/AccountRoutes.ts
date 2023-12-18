cdd0f740d7be5b63392e74a9d0f5171c2aefe7b9
LCM <10billionmin@naver.com>
2023-12-18 16:03:45 +0900

    account 로직 중단

--- src/routes/AccountRoutes.ts
+++ src/routes/AccountRoutes.ts
@@ -6,28 +6,32 @@ import logger from '../public/modules/jet-logger/lib/index';
 const router = express.Router();
 
 //오후 14시, Account 정보 magration
-if(process.env.INSTANCE_ID === '1'){
-    schedule.scheduleJob('0 0 14 * * *', async () => {
-        try {
-            logger.settings.filepath = `./LGE_logs/account/${utils.getToday()}_jet-logger.log`
-    
-            await AccountController.DB_to_Account(); 
-    
-        } catch (error) {
-            logger.err('AccountController.DB_to_Account schedule 중 오류:', error);
-        }
-    });
-}
+/*
+** App 연동으로 인하여 Account magration 중단 (2023-12-18) 
+*/
 
-router.post('/bulktest', async (req: Request, res: Response):Promise<void> => {
-    try{
-         await AccountController.DB_to_BulkAccount();
-    }catch(error){
-        logger.err('### Error /bulktest ###');
-        logger.err(error);
-        res.json(error.message);
-    }
-})
+// if(process.env.INSTANCE_ID === '1'){
+//     schedule.scheduleJob('0 0 14 * * *', async () => {
+//         try {
+//             logger.settings.filepath = `./LGE_logs/account/${utils.getToday()}_jet-logger.log`
+    
+//             await AccountController.DB_to_Account(); 
+    
+//         } catch (error) {
+//             logger.err('AccountController.DB_to_Account schedule 중 오류:', error);
+//         }
+//     });
+// }
+
+// router.post('/bulktest', async (req: Request, res: Response):Promise<void> => {
+//     try{
+//          await AccountController.DB_to_BulkAccount();
+//     }catch(error){
+//         logger.err('### Error /bulktest ###');
+//         logger.err(error);
+//         res.json(error.message);
+//     }
+// })
 //router.post('/update', AccountController.DB_to_Account);
 
 //router.get('/test', AccountController.test);
